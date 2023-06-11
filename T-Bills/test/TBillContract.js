929813c98c6dc328a813439a9b9771d0eaace796
Corey Feng <corey.feng@fazzfinancial.com>
2023-06-11 11:31:25 +0800

    Make test work

--- T-Bills/test/TBillContract.js
+++ T-Bills/test/TBillContract.js
@@ -5,11 +5,11 @@ const { expect } = require("chai");
 describe("TBillContract", function () {
   it("Should return correct constants", async function () {
     const [interestFund] = await ethers.getSigners();
-    const erc20TokenContract = await ethers.getContractFactory("ERC20TokenContract", "XSGD", "XSGD");
+    const erc20TokenContract = await ethers.deployContract("ERC20TokenContract", ["XSGD", "XSGD", 0]);
     const tbillContract = await ethers.deployContract("TBillContract", [interestFund.address, erc20TokenContract.address]);
 
     expect(await tbillContract.interestRateDecimals()).to.equal(6);
     expect(await tbillContract.interestFundAddress()).to.equal(interestFund.address);
-    console.log(await tbillContract.getERC20TokenAddress());
+    expect(await tbillContract.erc20TokenAddress()).to.equal(erc20TokenContract.address);
   });
 });
